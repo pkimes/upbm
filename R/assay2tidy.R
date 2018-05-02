37698ae637a4946a9ad486df0df93094b4984f09
Patrick Kimes <patrick.kimes@gmail.com>
2018-05-01 20:02:36 -0400

    Add helper to convert assays to tibbles

---
+++ R/assay2tidy.R
@@ -0,0 +1,49 @@
+#' SummarizedExperiment Assay to Tidy Object
+#'
+#' Simple helper function to convert assay format data in SummarizedExperiment
+#' object into a tidy tibble/data.frame for interactive analysis.
+#' 
+#' @param se SummarizedExpierment object.
+#' @param assay_name string name of the assay to use. (default = "fore")
+#' @param long logical whether to transform data to long format and
+#'        include colData in output rather than default wide format with
+#'        dimension similar to original SummarizedExperiment object.
+#'        (default = FALSE)
+#' @param .filter integer specifying level of probe filtering to
+#'        perform prior to plotting. (default = 1)
+#'
+#' @return
+#' tibble with assay data.
+#'
+#' @importFrom dplyr as_tibble bind_cols left_join
+#' @importFrom tibble rownames_to_column
+#' @importFrom tidyr gather
+#' @import SummarizedExperiment
+#' @export
+#' @author Patrick Kimes
+assay2tidy <- function(se, assay_name = "fore", long = FALSE, .filter = 1L) {
+
+    ## filter probes
+    se <- pbmFilterProbes(se, .filter) 
+
+    ## extract row data
+    rowdat <- as.data.frame(rowData(se), optional = TRUE)
+    rowdat <- dplyr::as_tibble(rowdat)
+    
+    ## extract intensities
+    pdat <- assay(se, assay_name)
+    pdat <- as.data.frame(pdat, optional = TRUE)
+    pdat <- dplyr::as_tibble(pdat)
+    pdat <- dplyr::bind_cols(pdat, rowdat)
+
+    if (long) {
+        ## extract column data
+        coldat <- as.data.frame(colData(se), optional = TRUE)
+        coldat <- tibble::rownames_to_column(coldat, "cname")
+
+        pdat <- tidyr::gather(pdat, cname, value, colnames(se))
+        pdat <- dplyr::left_join(pdat, coldat, by = "cname")
+    }
+    
+    return(pdat)
+}
