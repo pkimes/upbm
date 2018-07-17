18e9b22ecc1cc2b2cc8be96560e49070387cb7a8
Patrick Kimes <patrick.kimes@gmail.com>
2018-07-17 16:30:18 -0400

    Add 8x15k, 4x44k designs as tables

--- R/data-pbm_8x60k_v1.R
+++ R/data-pbm_8x60k_v1.R
@@ -1,7 +1,44 @@
-#' Standard PBM 8x60k Design
+#' Standard PBM Designs
 #'
-#' @name pbm_8x60k_v1
+#' Designs for standard PBM arrays are provided for convenience. For each
+#' spot, the Row, Column, ID and Sequence information is provided.
+#' 
+#' @format data.frame with each row corresponding to a spot on a PBM array
+#' @name pbm_designs
 #' @docType data
 #' @keywords data
+#' @source \url{http://the_brain.bwh.harvard.edu/PBMAnalysisSuite/}
 NULL
+
+#' @rdname pbm_designs
+#' @name pbm_8x60k_v1
+#' @section Designs:
+#'   pbm_8x60k_v1: Agilent array design 030265
+NULL
+
+#' @rdname pbm_designs
+#' @name pbm_4x44k_v1
+#' @section Designs:
+#'   pbm_4x44k_v1: Agilent array design: 051681 
+NULL
+
+#' @rdname pbm_designs
+#' @name pbm_4x44k_v2
+#' @section Designs:
+#'   pbm_4x44k_v2: Agilent array design: 016060 
+NULL
+
+#' @rdname pbm_designs
+#' @name pbm_8x15k_v1
+#' @section Designs:
+#'   pbm_8x15k_v1: Agilent array design: 015803
+NULL
+
+#' @rdname pbm_designs
+#' @name pbm_8x15k_v2
+#' @section Designs:
+#'   pbm_8x15k_v2: Agilent array design: 016236 
+NULL
+
+
 
