6fc9967b386490a9d174421c3f5a7e2426f1ce16
Patrick Kimes <patrick.kimes@gmail.com>
2019-02-05 11:35:26 -0500

    Change pbmFilterProbes to work with PBMExperiment,PBMDesign classes

--- R/pbmFilterProbes.R
+++ R/pbmFilterProbes.R
@@ -1,52 +1,56 @@
-#' Filter PBM Probes
+#' Filter PBMExperiment and PBMDesign objects
 #'
 #' Not all probes in a PBM experiment should be used for analysis.
-#' Given a SummarizedExperiment containing PBM intensities,
-#' this functions provides multiple levels of filtering to remove
-#' uninformative and low quality probes based on quality control
-#' metrics.
+#' Given a PBMExperiment or a PBMDesign object with defined probe
+#' filters, this function returns the subset of probes passing all
+#' filtering criteria.
 #' 
-#' @param se SummarizedExperiment object containing GPR
-#'        intensity information or DataFrame/data.frame object
-#'        containing the probe design.
-#' @param level integer specifying level of probe filtering to
-#'        perform prior to plotting. (default = 0)
+#' @param x a PBMExperiment or PBMDesign object
 #'
 #' @return
-#' SummarizedExperiment object.
+#' Filtered object of same class as input \code{se}.
 #'
-#' @details
-#' The function supports the following levels of filtering.
-#' All filtering is progressive, i.e. setting level = 2 will
-#' also include filtering at level = 1.
-#' * **0**: no filtering.
-#' * **1**: filter on de Bruijn probes.
-#'
-#' @md
 #' @export
 #' @author Patrick Kimes
-pbmFilterProbes <- function(se, level = 0L) {
-
-    if (is(se, "SummarizedExperiment")) {
-        pd <- rowData(se)
-    } else if (is(se, "DataFrame") | is(se, "data.frame")) {
-        pd <- se
+pbmFilterProbes <- function(x) {
+    if (!is(x, "PBMExperiment") & !is(x, "PBMDesign"))
+        stop("Probe filtering can only be performed with 'PBMExperiment' and 'PBMDesign' objects.")
+    
+    filters <- x@probeFilter
+    if (is(x, "PBMExperiment")) {
+        pddata <- rowData(x)[, x@probeCols, drop = FALSE]
     } else {
-        stop("se must be a SummarizedExperiment or DataFrame/data.frame")
+        pddata <- x@design
     }
 
-    if (level > 0L && ! "ID" %in% names(pd)) {
-        stop(paste0("Must have 'ID' column ", ifelse(is(se, "SummarizedExperiment"), "in rowData ", ""),
-                    "to use filter level > 0.\n"),
-             "If **all** probes should be used, set filter level to 0.\n",
-             "If only a subset of probes (i.e. de Bruijn probes) should be used, ",
-             "add 'ID' column.")
-        level <- 0L
+    fres <- mapply(function(fn, cl) { fn(pddata[[cl]]) },
+                   fn = filters, cl = names(filters),
+                   SIMPLIFY = FALSE)
+
+    if (any(vapply(fres, length, numeric(1)) != nrow(pddata))) {
+        stop("probeFilter functions should return logical vectors of length equal to number of probes.\n",
+             "number of probes = ", nrow(pddata), "\n",
+             "length of probeFilter output. \n",
+             paste0(paste0("    ", names(filters), " = ", vapply(fres, length, numeric(1))),
+                    collapse = "\n"))
     }
-    
-    if (level > 0L) {
-        se <- se[grepl("^dBr_", pd$ID), , drop = FALSE]
+    if (!all(vapply(fres, is, logical(1), class2 = "logical"))) {
+        stop("probeFilter functions should return logical vectors of length equal to number of probes.\n",
+             "number of probes = ", nrow(pddata), "\n",
+             "length of probeFilter output. \n",
+             paste0(paste0("    ", names(filters), " = ", vapply(fres, class, character(1))),
+                    collapse = "\n"))
     }
+
+    ## combine filtering results
+    fres <- simplify2array(fres)
+    fres <- apply(fres, 1, all)
     
-    return(se)
+    ## filter according to probeFilter rules
+    if (is(x, "PBMExperiment")) {
+        x <- x[fres, , drop = FALSE]
+    } else {
+        x@design <- x@design[fres, , drop = FALSE]
+    }
+    return(x)
 }
