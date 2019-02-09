02e07757397d03922a020ad5ce52a890a38709b9
Patrick Kimes <patrick.kimes@gmail.com>
2019-02-09 15:24:49 -0500

    Update cy3 residual calculators to use probeCols, return PBM classes

--- R/cy3FitModel.R
+++ R/cy3FitModel.R
@@ -1,4 +1,4 @@
-#' @title Compute Cy3 Scaling Factors using OLS
+#' @title Compute Cy3 scaling factors using regression
 #'
 #' @description
 #' PBM arrays are scanned twice, once for Cy3-tagged dUTPs to quantify
@@ -6,78 +6,93 @@
 #' protein. The Cy3 scans can be used to first filter out probes which
 #' appear to have poor dsDNA enrichment, and second, to scale Alexa488
 #' intensities to account for differences in dsDNA abundance between
-#' probes. Since probe intensities in the Cy3 scans are roughly
-#' proportional to the number of adenines in the probe sequence, the
-#' original Universal PBM Analysis Suite proposed estimating scaling factors
-#' by looking at the residuals of a linear regression fit using all
-#' tri-nucleotide sequences starting with an adenine. Given a
-#' SummarizedExperiment of Cy3 scans, this function fits the tri-nucleotide
-#' linear regression models and returns the expected Cy3 intensities,
-#' the residuals from the fits, and the corresponding observed-expected
-#' ratios for each probe as new assays added to the original Cy3
-#' SummarizedExperiment object.
+#' probes.
 #'
-#' The returned SummarizedExperiment object can be passed to \code{cy3Normalize}
+#' \emph{Both this function and the \code{cy3FitEmpirical} function can
+#' be used to estimate scaling factors for filtering and scaling
+#' Alexa488 probe intensities using Cy3 information. The \code{cy3FitEmpirical}
+#' approach is recommended when an appropriate reference set of Cy3 scans
+#' are available.}
+#'
+#' Since probe intensities in the Cy3 scans are roughly
+#' proportional to the number of adenines in the probe sequence, the
+#' original Universal PBM Analysis Suite proposed modeling the observed Cy3
+#' intensities with linear regression using the counts of all tri-nucleotide sequences
+#' starting with an adenine as covariates. The observed-to-expected intensity ratios
+#' are then used as the scaling factors.
+#'
+#' Given a PBMExperiment of Cy3 scans, this function fits the tri-nucleotide
+#' regression models and returns the expected Cy3 intensities,
+#' the residuals from the fits, and the corresponding observed-to-expected
+#' ratios for each probe as new assays added to the original Cy3
+#' PBMExperiment object.
+#'
+#' The returned PBMExperiment object can be passed to \code{cy3Normalize}
 #' to filter low quality probes and/or normalize Alexa488 intensities by the
 #' computed ratios.
 #' 
-#' @param se SummarizedExperiment object containing PBM Cy3 intensity data.
+#' @param pe a PBMExperiment object containing Cy3 intensity data.
 #' @param assay string name of the assay to use.
-#'        (default = \code{SummarizedExperiment::assayNames(se)[1]})
+#'        (default = \code{SummarizedExperiment::assayNames(pe)[1]})
 #' @param refit logical whether to filter outliers and refit trinucleotide
 #'        linear regression model. (default = TRUE)
-#' @param .filter integer specifying level of probe filtering to
-#'        perform prior to estimating affinities. See \code{pbmFilterProbes}
-#'        for more details on probe filter levels. (default = 1)
-#' @param .trim interger vector of length two specifying start and end
-#'        of probe sequence to be used. Default is based on the universal
-#'        PBM probe design where only leading 36nt should be used. 
-#'        Ignored if NULL. (default = c(1, 36))
+#' @param threshold a numeric threshold on absolute value of log2 ratio between
+#'        observed and expected Cy3 intensities. (default = 1L)
 #'
 #' @return
-#' original SummarizedExperiment object with additional assays corresponding
-#' to the ratio of observed vs. expected probe intensities, and whether probes were
-#' flagged as low-quality based on log2(ratio) > 1 or < -1. Cy3 models are stored
-#' in the metadata of the returned object.
+#' Original Cy3 PBMExperiment object with additional assays corresponding
+#' to ratio of observed to expected probe intensities, and whether probes were
+#' flagged as low-quality based on \code{abs(log2(ratio)) > threshold}.
+#' Cy3 models are stored in the metadata of the returned object.
 #'
 #' @seealso cy3Normalize cy3FitEmpirical
 #' @importFrom stats lm na.exclude predict
-#' @importFrom dplyr as_tibble select bind_cols group_by do ungroup left_join mutate
+#' @importFrom dplyr as_tibble select select_at bind_cols group_by do ungroup left_join mutate starts_with
 #' @importFrom tidyr gather spread
 #' @importFrom Biostrings DNAStringSet oligonucleotideFrequency
 #' @export
 #' @author Patrick Kimes
-cy3FitModel <- function(se, assay = SummarizedExperiment::assayNames(se)[1],
-                        refit = TRUE, .filter = 1L, 
-                        .trim = if (.filter > 0L) { c(1, 36) } else { NULL }) {
-    
+cy3FitModel <- function(pe, assay = SummarizedExperiment::assayNames(pe)[1],
+                        refit = TRUE, threshold = 1L, verbose = FALSE) {
+    stopifnot(is(pe, "PBMExperiment")) 
+
     ## check Sequence info in rowData
-    nse <- checkProbeSequences(se, FALSE)
+    npe <- checkProbeSequences(pe, FALSE)
 
     ## filter probes
-    nse <- pbmFilterProbes(nse, .filter)
+    npe <- pbmFilterProbes(npe)
 
     ## trim probe sequences
-    nse <- trimProbeSequences(nse, c(1, 36))
+    npe <- trimProbeSequences(npe)
+
+    if (verbose) {
+        cat("|| upbm::cy3FitModel \n")
+        cat("|| - Starting calculation of Cy3 deviations from OLS fit",
+            "for", ncol(pe), "Cy3 PBM scans.\n")
+    }
 
     ## determine row, sequence metadata
-    rowdat <- as.data.frame(rowData(nse), optional = TRUE)
+    rowdat <- as.data.frame(rowData(npe), optional = TRUE)
     rowdat <- dplyr::as_tibble(rowdat)
-    rowdat <- dplyr::select(rowdat, Row, Column, Sequence)
+    rowdat <- dplyr::select_at(rowdat, npe@probeCols)
 
     nt_freq <- Biostrings::DNAStringSet(rowdat$Sequence)
     nt_freq <- Biostrings::oligonucleotideFrequency(nt_freq, 3)
     nt_freq <- dplyr::as_tibble(nt_freq)
-    nt_freq <- dplyr::select(nt_freq, starts_with("A"))
+    nt_freq <- dplyr::select(nt_freq, dplyr::starts_with("A"))
 
     rowdat <- dplyr::bind_cols(rowdat, nt_freq)
 
     ## extract intensities
-    pdat <- SummarizedExperiment::assay(nse, assay)
+    pdat <- SummarizedExperiment::assay(npe, assay)
     pdat <- as.data.frame(pdat, optional = TRUE)
     pdat <- dplyr::as_tibble(pdat)
     pdat <- dplyr::bind_cols(pdat, rowdat)
-    pdat <- tidyr::gather(pdat, condition, intensity, colnames(se))
+    pdat <- tidyr::gather(pdat, condition, intensity, colnames(pe))
+
+    if (verbose) {
+        cat("|| - Fitting trinucleotide models to probes.\n")
+    }
 
     ## fit models
     pdat <- dplyr::group_by(pdat, condition)
@@ -93,33 +108,37 @@ cy3FitModel <- function(se, assay = SummarizedExperiment::assayNames(se)[1],
     pexps <- dplyr::left_join(pfits, tidyr::nest(pdat, -condition), by = "condition")
     pexps <- dplyr::mutate(pexps, preds = mapply(predict, object = fit, newdata = data,
                                                  SIMPLIFY = FALSE),
-                           coords = lapply(data, `[`, c("Row", "Column")))
-    pexps <- dplyr::select(pexps, condition, preds, coords)
+                           probecols = lapply(data, `[`, npe@probeCols))
+    pexps <- dplyr::select(pexps, condition, preds, probecols)
     pexps <- tidyr::unnest(pexps)
     pexps <- tidyr::spread(pexps, condition, preds)
 
     ## compute ratios
-    pratios <- tidyr::gather(pexps, condition, preds, -Row, -Column)
-    pratios <- dplyr::left_join(pratios, dplyr::select(pdat, Row, Column, condition, intensity),
-                                by = c("Row", "Column", "condition"))
+    pratios <- tidyr::gather(pexps, condition, preds, -(!! npe@probeCols))
+    pratios <- dplyr::left_join(pratios, dplyr::select_at(pdat, c("condition", "intensity", npe@probeCols)),
+                                by = c("condition", npe@probeCols))
     pratios <- dplyr::mutate(pratios, ratio = intensity / preds)
-    pratios <- dplyr::select(pratios, Row, Column, condition, ratio)
+    pratios <- dplyr::select_at(pratios, c("condition", "ratio", npe@probeCols))
     pratios <- tidyr::spread(pratios, condition, ratio)
 
     ## compute low quality probes (outside 2-fold difference)
-    pdrop <- tidyr::gather(pratios, condition, ratio, -Row, -Column)
-    pdrop <- dplyr::mutate(pdrop, lowq = ratio > 2 | ratio < 1/2)    
-    pdrop <- dplyr::select(pdrop, Row, Column, condition, lowq)
+    pdrop <- tidyr::gather(pratios, condition, ratio, -(!! npe@probeCols))
+    pdrop <- dplyr::mutate(pdrop, lowq = ratio > 2^threshold | ratio < 2^(-threshold))
+    pdrop <- dplyr::select_at(pdrop, c("condition", "lowq", npe@probeCols))
     
     ## refit data excluding outliers
     if (refit) {
         ## add initial fits to metadata for reference
         initial_fits <- pfits$fit
         names(initial_fits) <- pfits$condition
-        metadata(se)$cy3models_init <- initial_fits
+        metadata(pe)$cy3models_init <- initial_fits
 
+        if (verbose) {
+            cat("|| - Refitting trinucleotide models to probes passing filtering cutoff.\n")
+        }
+        
         ## refit models after setting outliers to NA
-        pdat <- dplyr::left_join(pdat, pdrop, by = c("Row", "Column", "condition"))
+        pdat <- dplyr::left_join(pdat, pdrop, by = c("condition", npe@probeCols))
         pdat <- dplyr::mutate(pdat, intensity = ifelse(lowq, NA, intensity))
         pdat <- dplyr::group_by(pdat, condition)
         pfits <- dplyr::do(pdat,
@@ -134,45 +153,48 @@ cy3FitModel <- function(se, assay = SummarizedExperiment::assayNames(se)[1],
         pexps <- dplyr::left_join(pfits, tidyr::nest(pdat, -condition), by = "condition")
         pexps <- dplyr::mutate(pexps, preds = mapply(predict, object = fit, newdata = data,
                                                      SIMPLIFY = FALSE),
-                               coords = lapply(data, `[`, c("Row", "Column")))
-        pexps <- dplyr::select(pexps, condition, preds, coords)
+                               probecols = lapply(data, `[`, npe@probeCols))
+        pexps <- dplyr::select(pexps, condition, preds, probecols)
         pexps <- tidyr::unnest(pexps)
         pexps <- tidyr::spread(pexps, condition, preds)
 
         ## re-compute ratios
-        pratios <- tidyr::gather(pexps, condition, preds, -Row, -Column)
-        pratios <- dplyr::left_join(pratios, dplyr::select(pdat, Row, Column, condition, intensity),
-                                    by = c("Row", "Column", "condition"))
+        pratios <- tidyr::gather(pexps, condition, preds, -(!! npe@probeCols))
+        pratios <- dplyr::left_join(pratios, dplyr::select_at(pdat, c("condition", "intensity", npe@probeCols)),
+                                    by = c("condition", npe@probeCols))
         pratios <- dplyr::mutate(pratios, ratio = intensity / preds)
-        pratios <- dplyr::select(pratios, Row, Column, condition, ratio)
+        pratios <- dplyr::select_at(pratios, c("condition", "ratio", npe@probeCols))
         pratios <- tidyr::spread(pratios, condition, ratio)
     }
     pdrop <- tidyr::spread(pdrop, condition, lowq)
     
     ## left join to original rowData to get full set
-    full_rowdat <- as.data.frame(rowData(se), optional = TRUE)
+    full_rowdat <- as.data.frame(rowData(pe), optional = TRUE)
     full_rowdat <- dplyr::as_tibble(full_rowdat)
-    full_rowdat <- dplyr::select(full_rowdat, Row, Column, Sequence)
-    pexps <- dplyr::left_join(dplyr::select(full_rowdat, Row, Column), pexps, by = c("Row", "Column"))
-    pratios <- dplyr::left_join(dplyr::select(full_rowdat, Row, Column), pratios, by = c("Row", "Column"))
-    pdrop <- dplyr::left_join(dplyr::select(full_rowdat, Row, Column), pdrop, by = c("Row", "Column"))
-
+    full_rowdat <- dplyr::select_at(full_rowdat, npe@probeCols)
+    pexps <- dplyr::left_join(dplyr::select_at(full_rowdat, npe@probeCols), pexps, by = npe@probeCols)
+    pratios <- dplyr::left_join(dplyr::select_at(full_rowdat, npe@probeCols), pratios, by = npe@probeCols)
+    pdrop <- dplyr::left_join(dplyr::select_at(full_rowdat, npe@probeCols), pdrop, by = npe@probeCols)
+    
     pexps <- DataFrame(pexps, check.names = FALSE)
-    pexps <- pexps[, rownames(colData(se)), drop = FALSE]
+    pexps <- pexps[, rownames(colData(pe)), drop = FALSE]
     pratios <- DataFrame(pratios, check.names = FALSE)
-    pratios <- pratios[, rownames(colData(se)), drop = FALSE]
+    pratios <- pratios[, rownames(colData(pe)), drop = FALSE]
     pdrop <- DataFrame(pdrop, check.names = FALSE)
-    pdrop <- pdrop[, rownames(colData(se)), drop = FALSE]
+    pdrop <- pdrop[, rownames(colData(pe)), drop = FALSE]
 
     ## add to SE object
-    SummarizedExperiment::assay(se, "expected") <- pexps
-    SummarizedExperiment::assay(se, "ratio") <- pratios
-    SummarizedExperiment::assay(se, "lowq") <- pdrop
+    SummarizedExperiment::assay(pe, "expected") <- pexps
+    SummarizedExperiment::assay(pe, "ratio") <- pratios
+    SummarizedExperiment::assay(pe, "lowq") <- pdrop
 
     ## add fits to metadata
     fits <- pfits$fit
     names(fits) <- pfits$condition
-    metadata(se)$cy3models <- fits
+    metadata(pe)$cy3models <- fits
 
-    return(se)
+    if (verbose) {
+        cat("|| - Finished calculation of Cy3 deviations.\n")
+    }
+    return(pe)
 }
--- R/cy3GenerateRef.R
+++ R/cy3GenerateRef.R
@@ -1,65 +1,67 @@
-#' Generate Cy3 Empirical Reference
+#' @title Generate Cy3 empirical reference
+#'
+#' @description
+#' Given a collection of Cy3 scans, this function estimates an empirical
+#' reference distribution of expected Cy3 intensities per probe. Scans
+#' must all be for the same array design. For each probe, the mean,
+#' median, MAD and standard deviation across scans is computed across all
+#' Cy3 scans.
+#'
+#' The estimated reference distributions can be passed to \code{cy3FitEmpirical}
+#' with Cy3 scan data to determine probe-level outliers and scaling factors
+#' for the individual Cy3 scans relative to the reference. 
+#'
+#' @param cy3pe a PBMExperiment object containing Cy3 intensity data.
+#' @param assay a numeric index or string name specifying the assay to use.
+#'        (default = \code{SummarizedExperiment::assayNames(cy3pe)[1]})
+#' @param offset a numeric offset to add to intensities before log2 transforming
+#'        to prevent NAs with zero intensities. (default = 1L)
+#' @param register logical whether to scale intensities across samples.
+#'        (default = TRUE)
 #' 
-#' Cy3 normalization is performed using an empirically-derived
-#' reference distribution computed using a large collection of
-#' samples. By default, samples are first scaled to have a common
+#' @return
+#' SummarizedExperiment object with Cy3 probe-level reference metrics. 
+#'
+#' @details
+#' By default, samples are first scaled to have a common
 #' median intensity, and the reference probe-level intensities are
 #' computed on the log2 scale. The reference is calculated on the
 #' log2 scale rather than the raw scale because the Cy3 scans will be
 #' used for filtering and scaling, actions which require examining
 #' fold changes and ratios rather than raw intensity differences.
 #'
-#' Scaling is performed by multiplying the raw intensities of each
-#' sample by a scaling factor such that the median intensity of each
+#' Scaling is performed such that the median intensity of each
 #' sample is equal to the median sample-median intensity of the original
 #' intensities. This multiplicative scaling is equivalent to an additive
-#' shift on the log2 scale.
-#'
-#' 
-#' @param cy3se SummarizedExperiment object containing Cy3 scan data.
-#' @param assay string name of assay to use. (default = \code{SummarizedExperiment::assayNames(cy3se)[1]})
-#' @param offset numeric offset to add to intensities before log2 transforming
-#'        to prevent errors with zero intensities. (default = 1L)
-#' @param register logical whether to scale intensities across samples.
-#'        (default = TRUE)
-#' @param .filter integer specifying level of probe filtering to
-#'        perform prior to estimating affinities. See \code{pbmFilterProbes}
-#'        for more details on probe filter levels. (default = 1)
-#' 
-#' @return
-#' SummarizedExperiment object with Cy3 probe-level reference metrics. 
-#' 
+#' shift on the log2 scale. This behavior can be turned off by specifying
+#' \code{register = FALSE}.
+#'  
 #' @export
 #' @importFrom stats mad median sd
-#' @importFrom dplyr group_by mutate ungroup summarize left_join select
+#' @importFrom dplyr group_by group_by_at mutate ungroup summarize left_join select
 #' @author Patrick Kimes
-cy3GenerateRef <- function(cy3se, assay = SummarizedExperiment::assayNames(cy3se)[1],
-                           offset = 1L, register = TRUE, .filter = 1L) {
+cy3GenerateRef <- function(cy3pe, assay = SummarizedExperiment::assayNames(cy3pe)[1],
+                           offset = 1L, register = TRUE) {
 
-    ## verify validity of cy3se
-    stopifnot(is(cy3se, "SummarizedExperiment"))
-    stopifnot("scan" %in% names(colData(cy3se)))
-    if (any(colData(cy3se)$scan != "Cy3")) {
-        stop("Not all scans in 'cy3se' are labelled as 'Cy3' in colData.\n",
-             "Only Cy3 scans should be passed to this function.")
-    }
+    ## verify validity of cy3pe
+    stopifnot(is(cy3pe, "PBMExperiment"))
     sum_cols <- paste0("probe_", c("median", "mean", "mad", "sd"))
-    if (any(names(rowData(cy3se)) %in% sum_cols)) {
-        stop("rowData of 'cy3se' has column names matching the following special names:\n",
+    if (any(names(rowData(cy3pe)) %in% sum_cols)) {
+        stop("rowData of 'cy3pe' has column names matching the following special names:\n",
              paste0(sum_cols, collapse = ", "), "\n",
              "Please rename these columns.")
     }
     
     ## tidy scan data for parsing
-    cy3vals <- tidy.SummarizedExperiment(cy3se, assay, long = TRUE, .filter = .filter)
+    cy3vals <- tidy.PBMExperiment(cy3pe, assay, long = TRUE)
     
     ## scale scans
     if (register) {
         ## common multiplication factor to scale samples
-        col_medians <- colMedians(as.matrix(SummarizedExperiment::assay(cy3se, assay)), na.rm = TRUE)
+        col_medians <- colMedians(as.matrix(SummarizedExperiment::assay(cy3pe, assay)), na.rm = TRUE)
         sfactor <- median(col_medians, na.rm = TRUE)
         
-        cy3vals <- dplyr::group_by(cy3vals, id, idx)
+        cy3vals <- dplyr::group_by(cy3vals, cname)
         cy3vals <- dplyr::mutate(cy3vals, value = value / median(value, na.rm = TRUE))
         cy3vals <- dplyr::ungroup(cy3vals)
         cy3vals <- dplyr::mutate(cy3vals, value = value * sfactor)
@@ -71,7 +73,7 @@ cy3GenerateRef <- function(cy3se, assay = SummarizedExperiment::assayNames(cy3se
     cy3vals <- dplyr::mutate(cy3vals, value = log2(value + offset))
 
     ## compute probe-level summary metrics
-    cy3vals <- dplyr::group_by(cy3vals, Column, Row)
+    cy3vals <- dplyr::group_by_at(cy3vals, .vars = cy3pe@probeCols)
     cy3vals <- dplyr::summarize(cy3vals,
                                 probe_median = median(value, na.rm = TRUE),
                                 probe_mean = mean(value, na.rm = TRUE),
@@ -80,8 +82,8 @@ cy3GenerateRef <- function(cy3se, assay = SummarizedExperiment::assayNames(cy3se
     cy3vals <- dplyr::ungroup(cy3vals)
 
     ## create SummarizedExperiment object
-    rowdat <- as.data.frame(rowData(cy3se), optional = TRUE)
-    cy3vals <- dplyr::left_join(dplyr::as_tibble(rowdat), cy3vals, by = c("Column", "Row"))
+    rowdat <- as.data.frame(rowData(cy3pe), optional = TRUE)
+    cy3vals <- dplyr::left_join(dplyr::as_tibble(rowdat), cy3vals, by = cy3pe@probeCols)
     rowdat <- DataFrame(dplyr::select(cy3vals, -one_of(sum_cols)))
     refassay <- dplyr::select(cy3vals, one_of(sum_cols))
     refassay <- list(ref = DataFrame(refassay))
@@ -91,6 +93,10 @@ cy3GenerateRef <- function(cy3se, assay = SummarizedExperiment::assayNames(cy3se
     params <- replace(formals(), names(params), params)
 
     ## return results as a SummarizedExperiment object
-    SummarizedExperiment(assays = refassay, rowData = rowdat,
-                         metadata = list(sfactor = sfactor, params = params))
+    PBMExperiment(assays = refassay, rowData = rowdat,
+                  metadata = list(sfactor = sfactor,
+                                  params = params),
+                  probeFilter = cy3pe@probeFilter,
+                  probeTrim = cy3pe@probeTrim,
+                  probeCols = cy3pe@probeCols)
 }
