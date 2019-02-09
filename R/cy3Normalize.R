79765906ae8537c8d74e20edbf0f820d1a909042
Patrick Kimes <patrick.kimes@gmail.com>
2019-02-09 15:54:04 -0500

    Update cy3Normalize output to add new assay, not replace

--- R/cy3Normalize.R
+++ R/cy3Normalize.R
@@ -1,5 +1,6 @@
-#' Perform Cy3 Normalization
+#' @title Perform Cy3 normalization
 #'
+#' @description
 #' Uses Cy3 intensities to quantify relative dsDNA material at each probe based
 #' on observed vs. expected intensity ratios.
 #' 
@@ -14,39 +15,26 @@
 #' @param filter logical whether to filter "low quality" flagged probes by setting to NA.
 #'        (default = TRUE)
 #' @param scale logical whether to perform scaling by ratio of observed vs. expected
-#'        Cy3 intensity at each probe. (default = FALSE)
+#'        Cy3 intensity at each probe. (default = TRUE)
 #' @param verbose a logical value whether to print verbose output during
 #'        analysis. (default = FALSE)
 #'
 #' @return
-#' PBMExperiment object.
+#' Original PBMExperiment object with additional assay corresponding to
+#' Cy3 filtered and/or scaled intensities (`normalized`).
 #'
-#' @seealso fitCy3Models
+#' @seealso cy3FitEmpirical
 #' @importFrom dplyr as_tibble mutate select left_join
 #' @importFrom tidyr gather spread
 #' @export
 #' @author Patrick Kimes
 cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)[1],
-                         match_by, filter = TRUE, scale = FALSE, verbose = FALSE) {
+                         match_by, filter = TRUE, scale = TRUE, verbose = FALSE) {
 
     stopifnot(is(pe, "PBMExperiment")) 
     stopifnot(is(cy3pe, "PBMExperiment")) 
     stopifnot("ratio" %in% SummarizedExperiment::assayNames(cy3pe))
     
-    if (verbose) {
-        cat("|| upbm::cy3Normalize ||\n")
-        cat("   | Starting probe-level Cy3 normalization and filtering",
-            "for", ncol(pe), "Alexa488 PBM scans.\n")
-    }
-
-    if (!filter & !scale) {
-        if (verbose) {
-            cat("   | No filtering or normalization performed.\n")
-            cat("   | Finished probe-level Cy3 normalization and filtering.\n")
-        }
-        return(pe)
-    }
-
     ## check validity of matching colData column
     stopifnot(match_by %in% names(colData(pe)))
     stopifnot(match_by %in% names(colData(cy3pe)))
@@ -64,6 +52,21 @@ cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)
     if (length(match_overlap) == 0) {
         stop("No samples matched across the specified two experiments.")
     }
+    
+    if (verbose) {
+        cat("|| upbm::cy3Normalize\n")
+        cat("|| - Starting probe-level Cy3 normalization and filtering",
+            "for", ncol(pe), "Alexa488 PBM scans.\n")
+    }
+
+    if (!filter & !scale) {
+        if (verbose) {
+            cat("|| - No filtering or normalization performed.\n")
+            cat("|| - Finished probe-level Cy3 normalization and filtering.\n")
+        }
+        return(pe)
+    }
+
     if (length(match_diff1) > 0) {
         warning("Dropping samples from pe:\n  ", paste(match_diff1, collapse = ", "))
     }
@@ -99,7 +102,7 @@ cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)
 
     if (scale) {
         if (verbose) {
-            cat("   | Performing Cy3 filtering ('filter = TRUE' specified).\n")
+            cat("|| - Performing Cy3 filtering ('filter = TRUE' specified).\n")
         }
         scale_assay <- as.data.frame(SummarizedExperiment::assay(cy3pe, "ratio"), optional = TRUE)
         scale_assay <- dplyr::as_tibble(scale_assay)
@@ -114,12 +117,12 @@ cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)
         new_assay <- dplyr::mutate(new_assay, value = value / scalar)
         new_assay <- dplyr::select(new_assay, -scalar)
     } else if (verbose) {
-        cat("   | No filtering performed ('filter = FALSE' specified).\n")
+        cat("|| - No filtering performed ('filter = FALSE' specified).\n")
     }
         
     if (filter) {
         if (verbose) {
-            cat("   | Performing Cy3 normalization ('scale = TRUE' specified).\n")
+            cat("|| - Performing Cy3 normalization ('scale = TRUE' specified).\n")
         }
         filter_assay <- as.data.frame(SummarizedExperiment::assay(cy3pe, "lowq"), optional = TRUE)
         filter_assay <- dplyr::as_tibble(filter_assay)
@@ -137,7 +140,7 @@ cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)
         new_assay <- dplyr::mutate(new_assay, value = ifelse(flag, NA, value))
         new_assay <- dplyr::select(new_assay, -flag)
     } else if (verbose) {
-        cat("   | No normalization performed ('scale = FALSE' specified).\n")
+        cat("|| - No normalization performed ('scale = FALSE' specified).\n")
     }
 
     ## return to square assay shape
@@ -155,11 +158,12 @@ cy3Normalize <- function(pe, cy3pe, assay = SummarizedExperiment::assayNames(pe)
     stopifnot(colnames(new_assay) %in% colnames(pe))
     new_assay <- new_assay[, colnames(pe)]
 
-    ## replace assay in pe object
-    SummarizedExperiment::assay(pe, assay) <- DataFrame(new_assay, check.names = FALSE)
-
+    ## add to input PBMExperiment
+    SummarizedExperiment::assays(pe) <- c(S4Vectors::SimpleList(normalized = DataFrame(new_assay, check.names = FALSE)),
+                                          SummarizedExperiment::assays(pe))
+    
     if (verbose) {
-        cat("   | Finished probe-level Cy3 normalization and filtering.\n")
+        cat("|| - Finished probe-level Cy3 normalization and filtering.\n")
     }
     return(pe)
 }
