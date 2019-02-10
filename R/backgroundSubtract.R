8f6686a7834d032b28ad00ba8848184ea71e3265
Patrick Kimes <patrick.kimes@gmail.com>
2019-02-10 12:26:45 -0500

    Drop complex limmaBackgroundCorrect fn, just keep simple subtraction

---
+++ R/backgroundSubtract.R
@@ -0,0 +1,74 @@
+#' Background subtract intensities
+#'
+#' Given a PBMExperiment with foreground and background intensities,
+#' this function performs simple background subtraction and returns the same
+#' PBMExperiment object with the background intensities subtracted from the
+#' foreground intensities.
+#'
+#' @param pe a PBMExperiment object containing PBM intensity data.
+#' @param assay a string name of the foreground assay to adjust.
+#'        (default = \code{SummarizedExperiment::assayNames(pe)[1]})
+#' @param assayb a string name of the background assay to use for adjustment.
+#'        (default = \code{SummarizedExperiment::assayNames(pe)[2]})
+#' @param keepb a logical value whether to keep the background assay
+#'        after subtraction. (default = TRUE)
+#' @param verbose a logical value whether to print verbose output
+#'        during analysis. (default = FALSE)
+#' @param nonnegative a logical whether to restrict intensities to non-negative
+#'        values by setting negative values to NA. (default = TRUE)
+#' 
+#' @return
+#' PBMExperiment object with background subtracted intensities replacing the original
+#' \code{assay} assay values.
+#' 
+#' @importFrom SummarizedExperiment assayNames assay
+#' @export 
+#' @author Patrick Kimes
+backgroundSubtract <- function(pe, assay = SummarizedExperiment::assayNames(pe)[1],
+                               assayb = SummarizedExperiment::assayNames(pe)[2],
+                               keepb = TRUE, verbose = FALSE, nonnegative = TRUE) {
+    stopifnot(is(pe, "PBMExperiment"))
+    stopifnot(assay %in% SummarizedExperiment::assayNames(pe))
+    stopifnot(assayb %in% SummarizedExperiment::assayNames(pe))
+
+    if (verbose) {
+        cat("|| upbm::backgroundSubstract \n")
+        cat("|| - Starting background subtraction for", ncol(pe), "PBM scans.\n")
+        cat("|| - Using foreground assay:", assay, "\n")
+        cat("|| - Using background assay:", assayb, "\n")
+    }
+    
+    ## perform quick subtraction
+    new_assay <- as.matrix(SummarizedExperiment::assay(pe, assay))
+    bkgd_assay <- as.matrix(SummarizedExperiment::assay(pe, assayb))
+    new_assay <- new_assay - bkgd_assay
+
+    if (nonnegative) {
+        if (verbose) {
+            cat("|| - Replacing all negative values after subtraction with NA (nonnegative = TRUE).\n")
+        }
+        new_assay[new_assay < 0] <- NA
+    } else if (verbose) {
+        cat("|| - Keeping all negative values after subtraction (nonnegative = FALSE).\n")
+    }
+    new_assay <- DataFrame(new_assay)
+    names(new_assay) <- rownames(colData(pe))
+    
+    if (verbose) {
+        cat("|| - Replacing values in", assay, "with subtracted intensities.\n")
+    }
+    ## modify input PBMExperiment
+    SummarizedExperiment::assay(pe, assay) <- new_assay
+
+    ## drop background assay unless dropping is specified
+    if (!keepb) {
+        if (verbose) {
+            cat("|| - Dropping background assay (keepb = FALSE).\n")
+        }
+        assay(pe, assayb) <- NULL
+    } else if (verbose) {
+        cat("|| - Keeping background assay (keepb = TRUE).\n")
+    }
+    
+    return(pe)
+}
