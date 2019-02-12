c6ed3bf55472c7a8ba211e02cb5a05943591309d
Patrick Kimes <patrick.kimes@gmail.com>
2019-02-12 12:09:42 -0500

    Update probeFit to work with new PBM classes

--- R/probeFit.R
+++ R/probeFit.R
@@ -1,49 +1,82 @@
-#' Fit Probe Models
+#' @title Fit probe models
 #'
 #' @description
-#' This function uses limma to estimate probe-level mean and moderated
-#' variance estimates across replicates within conditions.
+#' Given a PBMExperiment of normalized probe intensities, this function fits
+#' robust probe-level models across samples using the \code{lmFit} and \code{eBayes}
+#' functions from the \pkg{limma} package. The output is a PBMExperiment object with
+#' probes in rows and conditions, rather than individual samples, in columns. The
+#' condition labels of each sample should be specified as a column in the colData of
+#' input PBMExperiment using the \code{stratify=} parameter. By default, the function
+#' expects a \code{"condition"} column in the colData of the input object.
 #' 
-#' @param se SummarizedExperiment object containing PBM intensity data.
-#' @param assay string name of the assay to use. (default = \code{assayNames(se)[1]})
-#' @param groups unquoted name of column in colData of SummarizedExperiment (or
-#'        '\code{sample}') for grouping samples; values in columns will be
-#'        used as group labels. If no group labels are available, all samples
-#'        can be pooled by specifying \code{NULL}. (default = condition)
-#' @param offset integer offset to add to intensities before log2 scaling to
+#' @param pe a PBMExperiment object containing PBM intensity data.
+#' @param assay a string name of the assay to use for fitting probe models.
+#'        (default = \code{SummarizedExperiment::assayNames(pe)[1]})
+#' @param stratify a character string specifying a column in \code{colData(pe)} to
+#'        use for grouping samples, e.g. into . (default = \code{"condition"})
+#' @param offset an integer offset to add to intensities before log2 scaling to
 #'        prevent errors with zero intensities. If set to 0, probes with
 #'        zero intensities are dropped/ignored in estimation. (default = 1)
-#' @param .filter integer specifying level of probe filtering to
-#'        perform prior to estimating affinities. See \code{pbmFilterProbes}
-#'        for more details on probe filter levels. (default = 1)
-#' @param ... named arguments to be passed to \code{limma::eBayes}. See
-#'        Details for default parameters used in this function (different from
-#'        \code{limma::eBayes} default).
+#'        reference samples. (default = FALSE)
+#' @param verbose a logical value whether to print verbose output during analysis. (default = FALSE)
+#' @param ... additional parameters to be passed to \code{limma::eBayes}. See
+#'        Details for default parameters used in this function as they differ from
+#'        the \code{limma::eBayes} default values.
 #' 
 #' @return
-#' SummarizedExperiment object with probe-level intensity information aggregated
-#' across replicates. Each assay contains probe-level information for a single
-#' group in the specified \code{groups} variable.
+#' PBMExperiment object with probe-level log2 intensity information aggregated
+#' across replicates for each unique value in \code{stratify}. Columns in the returned
+#' PBMExperiment correspond to unique values of the \code{stratify} variable, and
+#' rows correspond to the subset of probes passing the filtering criteria
+#' of the original PBMExperiment object. The PBMExperiment includes three assays,
+#' \code{"beta"}, \code{"sd"}, and \code{"df"}, storing the cross-replicate
+#' mean, standard deviation, and degrees of freedom estimates for each condition and probe.
 #'
-#' @md
+#' @details
+#' More specifically, \code{limma::lmFit} is called on the log2-transformed values of the
+#' specified assay with no intercept and a binary design matrix with columns corresponding
+#' to the unique values of \code{colData(pe)[[stratify]]}. Since PBM experiments are often
+#' performed with few replicates per condition, the fit is then passed to \code{limma::eBayes}
+#' with \code{robust = TRUE} and \code{trend = TRUE} by default. Parameters to
+#' \code{limma::eBayes} can be modified by specifying them during the call to \code{probeFit}.
+#' 
+#' @references
+#' The cross-probe robust and trended variance estimates are obtained using the
+#' \code{eBayes} function from the limma package with \code{robust = TRUE} and
+#' \code{trend = TRUE} by default. The procedure was developed and described
+#' in the following paper:
+#' \itemize{
+#' \item Phipson, B., Lee, S., Majewski, I. J., Alexander, W. S., & Smyth, G. K. (2016). Robust hyperparameter estimation protects against hypervariable genes and improves power to detect differential expression. Annals of Applied Statistics, 10(2), 946-963.
+#' }
+#'
 #' @importFrom SummarizedExperiment SummarizedExperiment assayNames assay
 #' @importFrom dplyr left_join distinct
 #' @importFrom limma lmFit eBayes
+#' @importFrom purrr quietly
 #' @export
 #' @author Patrick Kimes
-probeFit <- function(se, assay = SummarizedExperiment::assayNames(se)[1],
-                     groups = condition, offset = 1L, .filter = 1L, ...) {
+probeFit <- function(pe, assay = SummarizedExperiment::assayNames(pe)[1],
+                     stratify = "condition", offset = 1L, verbose = FALSE, ...) {
+
+    stopifnot(is(pe, "PBMExperiment"))
+    stopifnot(assay %in% SummarizedExperiment::assayNames(pe))
+    stopifnot(is(offset, "numeric"))
     
+    if (verbose) {
+        cat("|| upbm::probeFit \n")
+        cat("|| - Starting probe-level model fitting for", ncol(pe), "PBM scans.\n")
+    }
+
     ## define eBayes parameters with defaults
     dots <- list(...)
     eb_args <- list(trend = TRUE, robust = TRUE)
     eb_args <- replace(eb_args, names(dots), dots)
 
     ## filter probes
-    se <- pbmFilterProbes(se, .filter) 
+    pe <- pbmFilterProbes(pe) 
 
     ## extract probe data matrix
-    datp <- SummarizedExperiment::assay(se, assay)
+    datp <- SummarizedExperiment::assay(pe, assay)
     datp <- log2(as.matrix(datp) + offset)
     datp[is.infinite(datp)] <- NA
     
@@ -54,57 +87,85 @@ probeFit <- function(se, assay = SummarizedExperiment::assayNames(se)[1],
         datp <- datp[!dropProbes, , drop = FALSE]
     }
 
+    if (verbose && (sum(dropProbes) > 0)) {
+        cat("|| - Skipping", sum(dropProbes), "probes with NAs across all samples.\n")
+    }
+    
     ## check stratification params
-    groups <- rlang::enquo(groups)
-    coldat <- .pbmCheckGroups(se, groups)
-        
+    coldat <- .pbmExpandStratify(pe, stratify)
+    
+    if (verbose) {
+        cat("|| - Fitting probe-level model with conditions:\n")
+        cat("||     -", paste0(colnames(coldat), collapse = ", "), "\n")
+    }
+
     ## fit limma model
-    fit <- limma::lmFit(datp, coldat)
+    fit <- purrr::quietly(limma::lmFit)(datp, coldat)
+    if (verbose && (length(fit$warnings) > 0L)) {
+        for (i in seq_len(length(fit$warnings))) {
+            cat("|| -", fit$warnings[i], "\n")
+        }
+    }
+    fit <- fit$result
+
+    if (verbose && (length(eb_args) > 0)) {
+        cat("|| - Performing eBayes shrinkage with:\n")
+        for (i in seq_len(length(eb_args))) {
+            cat("||     -", names(eb_args)[i], ":", eb_args[[i]], "\n") 
+        }
+    }
 
     ## fit empirical Bayes adjustment with specified parameters
     eb_args$fit <- fit
     fit <- do.call(limma::eBayes, eb_args)
-
+    
     ## return SummarizedExperiment with test results - one coef per assay
-    alist <- list(beta = fit$coefficients,
-                  sd = sweep(fit$stdev.unscaled, 1, fit$s2.post^.5, `*`),
-                  df = replicate(ncol(fit$coefficients), fit$df.total))
+    assayl <- list(beta = fit$coefficients,
+                   sd = sweep(fit$stdev.unscaled, 1, fit$s2.post^.5, `*`),
+                   df = replicate(ncol(fit$coefficients), fit$df.total))
 
     ## return to full dimensions
     if (any(dropProbes)) {
-        alist <- lapply(alist, function(x) {
+        assayl <- lapply(assayl, function(x) {
             y <- matrix(NA, nrow = nProbes, ncol = ncol(x))
             colnames(y) <- colnames(x)
             y[!dropProbes, ] <- x
             y
         })
     }
+
+    if (verbose) {
+        cat("|| - Finished probe-level model fitting.\n")
+    }
     
-    SummarizedExperiment(assays = alist, rowData = rowData(se))
+    PBMExperiment(assays = assayl,
+                  rowData = rowData(pe),
+                  probeCols = pe@probeCols,
+                  probeTrim = pe@probeTrim,
+                  probeFilter = pe@probeFilter)
 }
 
 
-## helper to check specified groups column in SummarizedExperiment object
-.pbmCheckGroups <- function(s, grp) {
-    grp_str <- rlang::quo_name(grp)
+## helper to return design matrix for specified stratify column in PBMExperiment object
+.pbmExpandStratify <- function(s, strat) {
 
     coldat <- data.frame(colData(s), check.names = FALSE,
                          check.rows = FALSE, stringsAsFactors = FALSE)
     coldat <- tibble::rownames_to_column(coldat, "sample")
 
     ## check validity of grouping colData column
-    if (! grp_str %in% names(colData(s))) {
-        if (grp_str != "NULL") {
-            stop("Specified Group must be unquoted column in colData or NULL.")
-        }
-        coldat <- dplyr::mutate(coldat, Groups = "allsamples")
+    if (is.null(strat)) {
+        coldat <- dplyr::mutate(coldat, Stratify = "allsamples")
+    } else if (! strat %in% names(colData(s))) {
+        stop("Specified \"stratify=\" argument should be a column in colData.\n",
+             "")
     } else {
-        coldat <- dplyr::mutate(coldat, Groups = !!grp)
+        coldat <- dplyr::rename(coldat, Stratify = I(strat))
     }
     
-    coldat <- dplyr::select(coldat, sample, Groups)
+    coldat <- dplyr::select(coldat, sample, Stratify)
     coldat <- dplyr::mutate(coldat, z = 1)
-    coldat <- tidyr::spread(coldat, Groups, z, fill = 0)
+    coldat <- tidyr::spread(coldat, Stratify, z, fill = 0)
     
     rord <- coldat$sample
     coldat <- dplyr::select(coldat, -sample)
